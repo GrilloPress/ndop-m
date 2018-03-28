51c7aed1f5b9136fc90e7e36f3669ae4a09321bc
grillopress <grillopress@gmail.com>
2018-03-28 14:38:03 +0100

    update references to confidential patient info

--- public/javascripts/M/06-confirm-your-choice.js
+++ public/javascripts/M/06-confirm-your-choice.js
@@ -10,7 +10,7 @@ function preferenceChecker() {
 
   // This is now an ugly hack...
 
-  var prefText = ( pref == "true"  ) ?  "<p>Your health data <strong>can</strong> be used | <a href='" + THE_PROTOTYPE_URL + "/app/05-make-your-choice' onclick='history.go(-1); return false;'>Change <span class='util-visuallyhidden'>your preference</span></a></p>" : "<p>Your health data <strong>cannot</strong> be used | <a href='" + THE_PROTOTYPE_URL + "/app/05-make-your-choice' onclick='history.go(-1); return false;'>Change <span class='util-visuallyhidden'>your preference</span></a></p>";
+  var prefText = ( pref == "true"  ) ?  "<p>Your confidential patient information <strong>can</strong> be used | <a href='" + THE_PROTOTYPE_URL + "/app/05-make-your-choice' onclick='history.go(-1); return false;'>Change <span class='util-visuallyhidden'>your preference</span></a></p>" : "<p>Your confidential patient information <strong>cannot</strong> be used | <a href='" + THE_PROTOTYPE_URL + "/app/05-make-your-choice' onclick='history.go(-1); return false;'>Change <span class='util-visuallyhidden'>your preference</span></a></p>";
 
   $( '#your-preference' ).after( prefText );
 
