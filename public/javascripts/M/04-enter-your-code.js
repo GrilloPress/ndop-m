a1f55c0d8ff50d9727aa622b59548dcf7e9d1a26
grillopress <grillopress@gmail.com>
2018-03-28 14:45:34 +0100

    check wording through system

--- public/javascripts/M/04-enter-your-code.js
+++ public/javascripts/M/04-enter-your-code.js
@@ -57,7 +57,7 @@ function submitCode() {
     // if 666 handle as error?
 
   } else {
-    window.location.href = THE_PROTOTYPE_URL + "/app/05-make-your-choice";
+    window.location.href = THE_PROTOTYPE_URL + "/app/05-set-your-preference";
   }
 
 }
